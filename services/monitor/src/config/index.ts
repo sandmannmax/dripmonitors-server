3624afcab33f7d041f5431fc9c46e4c157bd0645
sandmannmax <m1ng4@protonmail.com>
2021-01-24 17:08:45 +0100

    Removed monitor to add it as subrepo

--- services/monitor/src/config/index.ts
+++
@@ -1,8 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-export default {
-  dbConnection: process.env.DB_CONNECTION,
-  dbUser: process.env.MONGO_INITDB_ROOT_USERNAME,
-  dbPassword: process.env.MONGO_INITDB_ROOT_PASSWORD
-}
--- services/monitor/src/types/Monitor.ts
+++
@@ -1,7 +0,0 @@
-export class Monitor {
-  public _id: string;
-  public userId: string;
-  public webHook: string;
-  public botName: string;
-  public botImage: string;
-}
